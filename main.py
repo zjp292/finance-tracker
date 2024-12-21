049db8e196bf4ce36f5a8af753622dcc5669000a
Zachary Parham <zjp29@nau.edu>
2024-12-21 15:39:10 -0700

    created income vs expenses table

--- main.py
+++ main.py
@@ -10,35 +10,42 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import constants
 
+income_keywords = ['WELLS FARGO BANK PAYRLL', 'Aires', 'Zelle']
+
+
+# read csv data into dataframe
 df = pd.read_csv('activity-csv-data/Chase2809_Activity_20241216.CSV')
-df = df.dropna(axis=1)
-#print(df)
 
+# drop null/nan values
+df = df.dropna(axis=1)
+
+# change the posting date type to datetime
 df['Posting Date'] = pd.to_datetime(df['Posting Date'])
 
+# get year and month data
 df['Year'] = df['Posting Date'].dt.year
 df['Month'] = df['Posting Date'].dt.month
 
+
 monthly_data = df.groupby(['Year' , 'Month']).agg({'Amount': 'sum'}).reset_index()
-print(monthly_data)
+
 monthly_data['Year-Month'] = monthly_data['Year'].astype(str) + '-' + monthly_data['Month'].astype(str)
 
-income = df[df['Type'] == 'Credit'].groupby(['Year', 'Month'])['Amount'].sum()
-expenses = df[df['Type'] == 'Debit'].groupby(['Year', 'Month'])['Amount'].sum()
+
+#income = df[df['Description'].str.contains('|'.join(income_keywords))]
+income = df[(df['Details'] == 'CREDIT') &
+            df['Description'].str.contains('|'.join(income_keywords))]
+
+income = income.groupby(['Year', 'Month'])['Amount'].sum()
 
 
-plt.plot(monthly_data['Year-Month'], monthly_data['Amount'], marker='o')
-plt.title('Monthly Spending Trend')
-plt.xlabel('Month')
-plt.ylabel('Total Amount ($)')
-plt.xticks(rotation=45)
-plt.grid()
-plt.tight_layout()
-plt.show()
+expenses = df[(df['Details'] == 'DEBIT') & 
+              (~df['Description'].str.contains('Transfer', case=True, na=False))]
+expenses = expenses.groupby(['Year', 'Month'])['Amount'].sum()
 
+delta = income + expenses
 
-
-
+print('income: ', income)
+print('expenses: ', expenses)
+print('diff change: ', delta)
