4b6ee081fc55cb4759c3c8c09cff4d29ea71c4fd
zjp292 <zjp29@nau.edu>
2024-12-21 10:04:29 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# get most recent activity csv file
+    # create secondary script (host on a server?) 
+    # to remind me to get the most up to date data
+
+# parse through the data
+    # create spending / income break down
+    # visualize the data 
+
+# create spending categories possible
